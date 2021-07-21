4a35c63eed7338bbef916b8df33cb6b2a8964ee4
victor <vajor@mgic.umaryland.edu>
2021-07-21 01:34:14 +0100

    add the actors section

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,16 @@
 import React from 'react'
-// import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
-// import Grid from './Grid';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import Grid from './Grid';
 import Spinner from './Spinner';
 import { useParams } from 'react-router';
 
 import { useMovieFetch } from '../hooks/useMovieFetch';
 import BreadCrumb from './BreadCrumb';
 import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 
-// import NoImage from '../images/no_image.jpg';
+import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
     const {movieId} = useParams();
@@ -22,6 +24,23 @@ const Movie = () => {
         <>
             <BreadCrumb movieTitle={movie.original_title}/>
             <MovieInfo movie={movie} />
+            <MovieInfoBar 
+            time={movie.runtime} 
+            budget={movie.budget} 
+            revenue={movie.revenue} />
+            <Grid header='Actors'>
+                {movie.actors.map(actor => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                            : NoImage
+                        }
+                        />
+                ))}
+            </Grid>
         </>
     )
 }
